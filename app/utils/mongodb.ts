139b0fba0531d050d70b7109a453de37de50c221
Ayina Maerik <leghis@mac.home>
2024-11-05 21:42:01 -0500

    update

--- app/utils/mongodb.ts
+++ app/utils/mongodb.ts
@@ -1,7 +1,8 @@
+// utils/mongodb.ts
 import { MongoClient } from 'mongodb'
 
 if (!process.env.MONGODB_URI) {
-    throw new Error('Please add your Mongo URI to .env.local')
+    throw new Error('Please define the MONGODB_URI environment variable inside .env.local')
 }
 
 const uri = process.env.MONGODB_URI
@@ -17,7 +18,7 @@ let clientPromise: Promise<MongoClient>
 if (process.env.NODE_ENV === 'development') {
     // In development mode, use a global variable so that the value
     // is preserved across module reloads caused by HMR (Hot Module Replacement).
-    const globalWithMongo = global as typeof globalThis & {
+    let globalWithMongo = global as typeof globalThis & {
         _mongoClientPromise?: Promise<MongoClient>
     }
 
